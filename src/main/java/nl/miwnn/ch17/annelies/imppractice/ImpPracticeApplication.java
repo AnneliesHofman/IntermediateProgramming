c36289b0c8a41532fe29c125ea302d673828c931
Annelies Hofman <annelies.hofman.art@gmail.com>
2025-10-28 07:33:35 +0100

    initial commit

---
+++ src/main/java/nl/miwnn/ch17/annelies/imppractice/ImpPracticeApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.annelies.imppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpPracticeApplication.class, args);
+	}
+
+}
