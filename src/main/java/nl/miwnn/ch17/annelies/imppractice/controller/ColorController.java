af3ad3cad2b2b105ae12f90802cbc82dde17858f
Annelies Hofman <annelies.hofman.art@gmail.com>
2025-10-28 14:00:21 +0100

    Added a column to the /color page table, to display a gradient based on the color's values

--- src/main/java/nl/miwnn/ch17/annelies/imppractice/controller/ColorController.java
+++ src/main/java/nl/miwnn/ch17/annelies/imppractice/controller/ColorController.java
@@ -20,8 +20,8 @@ public class ColorController {
 
         ArrayList<Color> colors = new ArrayList<>();
 
-        colors.add(new Color("142;28;54", "161;59;27", "159;100;5"));
-        colors.add(new Color("72;52;75", "83;28;59", "124;72;16"));
+        colors.add(new Color("142, 28%, 54%", "161, 59%, 27%", "159, 100%, 5%"));
+        colors.add(new Color("72, 52%, 75%", "83, 28%, 59%", "124, 72%, 16%"));
 
         datamodel.addAttribute("colors", colors);
 
