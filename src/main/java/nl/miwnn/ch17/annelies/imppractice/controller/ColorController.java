d7788569d7b4e056c7f701229b08e08cff0df31f
Annelies Hofman <annelies.hofman.art@gmail.com>
2025-10-29 11:19:33 +0100

    Added form to add new color

--- src/main/java/nl/miwnn/ch17/annelies/imppractice/controller/ColorController.java
+++ src/main/java/nl/miwnn/ch17/annelies/imppractice/controller/ColorController.java
@@ -4,7 +4,10 @@ import nl.miwnn.ch17.annelies.imppractice.model.Color;
 import nl.miwnn.ch17.annelies.imppractice.repositories.ColorRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 
@@ -22,11 +25,27 @@ public class ColorController {
         this.colorRepository = colorRepository;
     }
 
-    @GetMapping("/colors")
+    @GetMapping({"/color/all", "/"})
     private String showColorOverview(Model datamodel) {
 
         datamodel.addAttribute("colors", colorRepository.findAll());
 
         return "colorOverview";
+    }
+
+    @GetMapping("/color/add")
+    public String showColorForm(Model datamodel) {
+        datamodel.addAttribute("formColor", new Color());
+
+        return "colorForm";
+    }
+
+    @PostMapping("/color/save")
+    public String saveOrUpdateColor(@ModelAttribute("formColor") Color color, BindingResult result) {
+        if (!result.hasErrors()){
+            colorRepository.save(color);
+        }
+
+        return "redirect:/color/all";
     }
 }
